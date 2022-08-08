0c971474399a9dbf000eca675c96aa4b96dcbe71
Alexandru Bacanu <alex.bacanu@live.com>
2022-08-08 21:00:10 +0300

    style: configure temporary navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div className="m-4">
+      <div className="flex items-center justify-center border-4 border-gray-200 border-dashed rounded-lg h-96">
+        <p className="text-3xl text-gray-300">Content</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
